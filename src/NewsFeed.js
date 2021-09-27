67e1204b7ffbd5c75a49175dc0f4ea2b6edfcdce
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-09-27 15:02:18 +0800

    updateNewsFeed

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -1,49 +1,51 @@
-/* eslint-disable no-unused-vars */
-import React, {useState, useEffect} from "react";
-/**
- * Request Top Headline from https://newsapi.org/
- * Show loading message while fetching
- * Handle Error 
- * Complete functionality of 'Load More' & 'Refresh' button
- */
+import React, {useState, useEffect} from 'react'
+/*
+  * Request Top Headline from https://newsapi.org/
+  * Show Loading message while fetching
+  * Handle Error
+  * Complete functionality of 'Load More' & 'Refresh' button
+*/
 
- const defaultNews= {
-   status: "ok",
-   totalResult: 0,
-   articles: []
- }
- const endpoint =
-   "https://newsapi.org/v2/top-headlines?country=id&apiKey=d4eef434bb2848cca69d497ef7eb2b42";
+const defaultNews = {
+  status :"ok",
+  totalResults : 0,
+  articles : []
+}
 
-function NewsFeed() {
-  const [news, setNews] = useState(defaultNews);
-  const [page,setPage] = useState(1)
+const endpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=4ed51672fb6e4b0cb518a3fbb6943443"
+
+export default function NewsFeed() {
+  
+  const [news, setNews] = useState(defaultNews) 
+  const [page , setPage] = useState(0)
   const [isLoading, setLoading] = useState(false)
-  const [isError, setError] = useState(false) 
+  const [isError, setError] = useState(false)
   const [isRefresh, setRefresh] = useState(false)
-  const handleRefresh = () => {
+
+  const handleRefresh = () =>{
     setNews(defaultNews)
     setPage(1)
     setLoading(false)
     setRefresh(false)
-  }  
-  useEffect(()=>{
+  }
+
+  useEffect(() => {
     const fetchData = async () => {
       setLoading(true)
+
       try {
-        const response = await fetch(`${endpoint}&page=${page}`)  
+        const response = await fetch(`${endpoint}&page=${page}`)
         const result = await response.json()
-        setNews(current=>{
-          return(
-            {
-              ...result,
-              articles: [...current.articles,...result.articles],
-              totalResult: result.totalResult,
-              status: result.status
-            }
-          )
+  
+        setNews (current => {
+          return ({
+            ...result,
+            articles : [...current.articles, ... result.articles],
+            totalResults : result.totalResults,
+            status : result.status
+          })
         })
-        if(result.status !== "ok"){
+        if (result.status !== "ok"){
           throw new Error('error')
         }
       } catch (error) {
@@ -52,30 +54,27 @@ function NewsFeed() {
       setLoading(false)
     }
     fetchData()
-  },[page,isRefresh])
+  }, [page, isRefresh])
+
   return (
-    <>
-      <h3>Top News Headline </h3>
-      {isLoading && <p> Loading...</p>}
-      {isError && <p> mon maap ada gangguan sis</p>}
+    <div>
+      <h3>Top News Headline</h3>
+      {isLoading && <p>Loading..</p>}
+      {isError && <p>Maaf bang ada gangguan</p>}
       <ol>
         {news.articles.map((item, index) => (
-          <li key={index}>
-            {" "}
-            <a href={item.url} target="blank">{item.title}</a>
-          </li>
+          <li key={index}> {item.title} </li>
         ))}
       </ol>
-      {news.articles.length < parseInt(news.totalResults) ? (
-        <button disabled={isLoading} onClick={() => setPage(c => c + 1)}>
-          Load More
-        </button>
-      ) : null}
-      <button onClick={handleRefresh} disabled={isLoading}>
-        Refresh{" "}
-      </button>
-    </>
-  );
+      {
+        news.articles.length < parseInt(news.totalResults) ? (
+          <button
+            disabled={isLoading}
+            onClick= { () => setPage(c => c+1)}
+          >Load More</button>
+        ) : null
+      }
+      <button onClick={handleRefresh} disabled={isLoading}>Refresh</button>
+    </div>
+  )
 }
-
-export default NewsFeed;
